b22c6f0d416a1ee586e340f6dc436dcf59d194ed
Felix Idalia <flxidalia@gmail.com>
2025-01-25 17:52:54 +0300

    Build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? '/my-portfolio/' : '',
+    basePath: isProd ? '/my-portfolio' : '',
+}
+    
 module.exports = nextConfig
